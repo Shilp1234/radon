01b0164dea29539c3d63fb11f4de5e534d17fdbe
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-06-12 09:00:35 +0530

    Update bookController.js

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,6 +1,6 @@
 const { count } = require("console")
 const AuthorModel = require("../models/authorModel")
-const BookModel= require("../models/authorModel")
+const BookModel= require("../models/bookModel")
 const mongoose = require('mongoose')
 
 /*-----------------------Include All Methods------------------------*/
@@ -30,6 +30,26 @@ let authorBooks = async function(req,res){
     let price=data.price
     res.send({msg:authorData,price})
 }
+const getBooksByAuthorId=async function(req,res){
+    let authorId=req.parms.requstID
+    let authorName=await bookModel.find({author_id}).select({novelName:1,_id:0})
+    console.log(authorName)
+    if(authorName.length===0){
+        res.send({msg:"No Books by this  Author Id"})
+    }else{
+        res.send({msg:authorName})
+    }
+}
+const getAuthorByAge=async function(req,res){
+    let novel=await bookModel.find({rating:{$gt:4}}).select({author_id:1,_id:0})
+    console.log(novel)
+    let authorInfo
+    for(let index=0;index<bookModel.length;index++){
+        authorInfo=await authorModel.find({$and:[{age:{$gt:50}},novel[index]]}
+            ).select({authorName:1,age:1,_id:0})}
+        res.send({msg:authorInfo})
+
+    }
 
 
 /*----------------------Here,import the function-----------------------*/
@@ -38,7 +58,8 @@ module.exports.createBook=createBook
 module.exports.createAuthor=createAuthor
 module.exports.getBooksByChetanBhagat=getBooksByChetanBhagat
 module.exports.authorBooks=authorBooks
-
+module.exports.getBooksByAuthorId=getBooksByAuthorId
+module.exports.getAuthorByAge=getAuthorByAge
 
 
 
