8e14beed9825e9c0b1682de00083e7812df83f1b
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-08-20 17:33:54 +0530

    Update publisherController.js

--- src/controllers/publisherController.js
+++ src/controllers/publisherController.js
@@ -1,15 +1,18 @@
 const PublisherModel= require("../models/publisherModel")
 
+//******************************Create Publisher************************************//
 const createPublisher= async function (req, res) {
     let publisherData = req.body
     let publisherCreated = await PublisherModel.create(publisherData)
     res.send({data: publisherCreated})
 }
+
+//******************************Find Publisher************************************//
 const getPublisherData= async function (req, res) {
     let publisher = await PublisherModel.find()
     res.send({data: publisher})
 }
 
-
+//******************************Decleration Publically************************************//
 module.exports.createPublisher= createPublisher
 module.exports.getPublisherData = getPublisherData
