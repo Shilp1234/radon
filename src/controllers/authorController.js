e6510b7935fe07edb303192d99b6f1ff83e6e1c0
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-08-20 17:28:35 +0530

    Update authorController.js

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,15 +1,25 @@
 const AuthorModel= require("../models/authorModel")
 
+//****************************** Find Author ************************************//
 const createAuthor= async function (req, res) {
     let author = req.body
     let authorCreated = await AuthorModel.create(author)
     res.send({data: authorCreated})
 }
 
+//****************************** Get Author Data ************************************//
 const getAuthorsData= async function (req, res) {
     let authors = await AuthorModel.find()
     res.send({data: authors})
 }
 
+//******************************Find Publisher************************************//
+const publisherData=async function(req, res){
+    let publisher=await AuthorModel.find()
+    res.send({data:publisherData})
+}
+//***************************Public Declaration This lines*************************//
 module.exports.createAuthor= createAuthor
 module.exports.getAuthorsData= getAuthorsData
+module.exports.publisherData=publisherData
+
